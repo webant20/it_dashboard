3e8f2935227ca988bac31fc219061c4183ddcbaf
it_admin <it_admin@development.myguest.virtualbox.org>
2025-02-26 01:41:13 +0530

    Save changes before implementing dynamic form customization

--- PlannedActivityApp/admin.py
+++ PlannedActivityApp/admin.py
@@ -18,3 +18,17 @@ class PlannedActivityAdmin(admin.ModelAdmin):
     list_display = ("periodic_activity", "due_date", "assigned_to", "status")
     list_filter = ("status", "due_date")
     search_fields = ("periodic_activity__name", "assigned_to__username")
+
+    def get_queryset(self, request):
+        """Restrict non-superusers to only see their assigned activities."""
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs  # Superusers see all records
+        return qs.filter(assigned_to=request.user)  # Non-superusers see only their assigned records
+
+    def get_form(self, request, obj=None, **kwargs):
+        """Restrict 'assigned_to' field visibility for non-superusers."""
+        form = super().get_form(request, obj, **kwargs)
+        if not request.user.is_superuser:
+            form.base_fields["assigned_to"].disabled = True
+        return form
